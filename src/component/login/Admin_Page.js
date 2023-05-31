ccc386391900b974115531581270867360c63811
babubhaya <zeeshankha434@gmail.com>
2023-05-31 05:13:58 -0700

    login page

--- src/component/login/Admin_Page.js
+++ src/component/login/Admin_Page.js
@@ -1,42 +1,84 @@
 import React, { useState } from 'react';
 import './admin.css';
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-import emi from "../../assets/email.svg"
+import emi from "../../assets/email.svg";
 import lock from '../../assets/lock.svg';
 import half from '../../assets/half.svg';
 import { colors } from '@mui/material';
+
 function Admin_Page() {
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [otpSent, setOtpSent] = useState(false);
+  const [enteredOTP, setEnteredOTP] = useState('');
+
+  const handlePhoneNumberChange = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const handleOTPChange = (event) => {
+    setEnteredOTP(event.target.value);
+  };
+
+  const handleSubmitPhoneNumber = (event) => {
+    event.preventDefault();
+
+    // Code to send OTP to the provided phone number (via API call, for example)
+    // Replace the following code with your implementation
+
+    // Simulating API call delay for demonstration purposes
+    setTimeout(() => {
+      setOtpSent(true);
+    }, 2000);
+  };
+
+  const handleSubmitOTP = (event) => {
+    event.preventDefault();
+
+    // Code to verify the entered OTP (via API call, for example)
+    // Replace the following code with your implementation
+
+    // Simulating API call delay for demonstration purposes
+    setTimeout(() => {
+      if (enteredOTP === '123456') {
+        alert('OTP verified successfully!');
+      } else {
+        alert('Incorrect OTP. Please try again.');
+      }
+    }, 2000);
+  };
+
   const [credentials, setCredentials] = useState({
-    email : undefined,
+    email: undefined,
     password: undefined,
   });
-  const [login,setLogin]=useState(false);
-  const [error,setError]=useState({});
+  const [login, setLogin] = useState(false);
+  const [error, setError] = useState({});
 
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
-  const {password,email}= credentials
+
+  const { password, email } = credentials;
+
   const handleClick = async (e) => {
     e.preventDefault();
     try {
       const res = await axios({
         method: 'post',
         url: 'http://81.0.219.62:3000/api/v1/admin/get-admin',
-        data:{
-            "email": `${email}`,
-            "password": `${password}`
-        }
+        data: {
+          email: `${email}`,
+          password: `${password}`,
+        },
       });
-      setError(res.data.data.data)
-      navigate('/user', { state: error }); 
-    
+      setError(res.data.data.data);
+      navigate('/user', { state: error });
     } catch (err) {
-        setLogin(true)
-     setError(err.response.data.message)
+      setLogin(true);
+      setError(err.response.data.message);
     }
   };
 
@@ -50,27 +92,49 @@ function Admin_Page() {
       </div>
 
       <div className='main-div'>
-        <div>
-          <label className="name-label  text-center">
-            <img className="icon " src={emi} />
-            <input type="text" id="email" name="email" placeholder="Enter your email" onChange={handleChange} className='inputlog' />
-          </label>
-        </div>
-        <div>
-          <label className="name-label  text-center">
-            <img className="icon" src={lock} />
-            <input type="password" id="password" name="password" placeholder="Enter your password" onChange={handleChange} className='inputlog' />
-          </label>
-        </div>
+        {!otpSent && (
+          <form onSubmit={handleSubmitPhoneNumber}>
+            <label className="name-label text-center">
+              <input
+                type="text"
+                className="form-control"
+                id="phoneNumber"
+                placeholder="Enter your phone number"
+                value={phoneNumber}
+                onChange={handlePhoneNumberChange}
+              />
+            </label>
+            <br></br>
+            <button type="submit" className="otp_button">
+              {otpSent ? 'Resend OTP' : 'Send OTP'}
+            </button>
+          </form>
+        )}
+
+        {otpSent && (
+          <form onSubmit={handleSubmitOTP}>
+            <h3 className="text-center enter_otp">Enter the OTP:</h3>
+            <label>
+              <input
+                type="text"
+                className="form-control"
+                id="otpInput"
+                placeholder="Enter the OTP"
+                value={enteredOTP}
+                onChange={handleOTPChange}
+                required
+              />
+            </label>
+            <br></br>
+            <button type="submit" className="otp_button">
+              Verify OTP
+            </button>
+          </form>
+        )}
       </div>
       {login && <h3 className='failed'>{error}</h3>}
-      <img className='half' src={half} />
-      <label className='login-button'>
-        <button onClick={handleClick} >Login</button>
-      </label>
-
     </div>
-  )
+  );
 }
 
-export default Admin_Page
+export default Admin_Page;
